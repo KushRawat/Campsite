c7d303a92409b0681a99e92d8de9b23da520ac69
Kush Rawat <rawatkush0704@gmail.com>
2022-03-03 01:11:35 +0530

    html and mongo sanitized

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user");
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require("./routes/user");
 const campgroundRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/review");
@@ -39,6 +41,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: "thisecretneedstobechanmged",
